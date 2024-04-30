bdd3cc357e2c2f3dc929321fe0160adff3151751
Irene Li <gnituyil@gmail.com>
2024-04-30 12:53:00 +0200

    update

---
+++ inference.py
@@ -0,0 +1,144 @@
+import numpy as np
+from scipy.optimize import minimize, brute
+from scipy.linalg import solve_continuous_lyapunov
+from scipy.stats import multivariate_normal
+import scipy
+
+class sim(): 
+    def __init__(self, params, convert_params):
+        self.J, B = convert_params(params)
+        self.rv =  multivariate_normal([0, 0], B)
+        
+    def evolve(self, T, dt, n_frames, n_repeats):
+        results = [] 
+        for n in range(n_repeats): 
+            y = np.zeros((2))
+            res = np.zeros((n_frames, 2))
+            for i in range(n_frames):
+                for j in range(int(T/n_frames/dt)):
+                    self._update(y, dt)
+                res[i] = np.copy(y)
+            results.append(res.T) 
+        return results 
+
+
+    def _update(self, y, dt):
+        det = self.J @ y * dt 
+        sto = np.sqrt(dt)*self.rv.rvs()
+        y += det + sto 
+
+class inference():
+    '''
+    basic inference class that assumes a linear 6 parameter model 
+    '''
+    
+    def __init__(self, convert_params):
+        self.convert_params = convert_params 
+    
+    def inf(self, guess, trajs, dt, bounds=None, brute_force=False, method='trust-constr'):   
+        cost = lambda x: self._minuslogP(x, trajs, dt)
+        if bounds is None: 
+            bounds = [[1e-10, None]]*len(guess)
+            bounds[1] = [None, None]
+        if brute_force: 
+            res = brute(cost, bounds, Ns=100)
+            MAP, cost = res[0], res[1]
+        else: 
+            res = minimize(cost, guess, bounds=bounds, method=method)
+            MAP, cost = res.x, res.fun 
+        return MAP, cost
+
+    def std_error(self, MAP, trajs, dt, delta=1e-3):
+        cost = lambda x: self._minuslogP(x, trajs, dt)
+        temp = np.copy(MAP)
+        if not isinstance(delta, list):
+            delta = MAP*delta 
+        hess = np.empty((len(MAP), len(MAP)))
+        for i in range(len(MAP)):
+            for j in range(i, len(MAP)):
+                temp[i] += delta[i]
+                temp[j] += delta[j]
+                hess[i, j] = cost(temp) # ++ 
+                temp[j] -= 2*delta[j]
+                hess[i, j] -= cost(temp) # +- 
+                temp[i] -= 2*delta[i]
+                hess[i, j] += cost(temp) # -- 
+                temp[j] += 2*delta[j]
+                hess[i, j] -= cost(temp) # -+
+                hess[i, j] /= (4*delta[i]*delta[j])
+                hess[j, i] = hess[i, j]   
+        return np.sqrt(np.diagonal(np.linalg.inv(hess))), hess
+
+    def _minuslogP(self, params, trajs, dt):
+        '''
+        traj: 2 x T  
+        '''
+        J, B = self.convert_params(params)
+        self._set_up(J, dt)
+        cov = self._cov(J, B) 
+        invcov = np.linalg.inv(cov)
+        sign, norm = np.linalg.slogdet(invcov)
+        
+        minuslogp = 0 
+        for traj in trajs: 
+            T = traj.shape[-1]-1
+            xm = self.evo @ traj[:, :-1]
+            diff = (traj[:, 1:] - xm)
+            minuslogp += np.einsum('ji,jk,ki', diff, invcov, diff).real/2 
+            minuslogp -= T*norm/2
+        return minuslogp
+
+    def _set_up(self, J, dt):
+        self.evo =  scipy.linalg.expm(J*dt)
+        
+    def _cov(self, J, B):
+        c = solve_continuous_lyapunov(J, -B)  
+        return - self.evo @ c @ self.evo.T + c 
+
+
+class exact_inference(inference): 
+    
+    def _minuslogP(self, J, B, trajs, dt):
+        self._set_up(J, dt)
+        invcov = np.linalg.inv(self._cov(J, B)) 
+        sign, norm = np.linalg.slogdet(invcov)
+        
+        minuslogp = 0 
+        for traj in trajs: 
+            T = traj.shape[-1]-1
+            xm = self.evo @ traj[:, :-1]
+            diff = (traj[:, 1:] - xm)
+            minuslogp += np.einsum('ji,jk,ki', diff, invcov, diff).real/2 
+            minuslogp -= T*norm/2
+        return minuslogp
+    
+    def inf(self, trajs, dt):
+
+        T1 = np.zeros((2, 2))
+        T2 = np.zeros((2, 2))
+        T3 = np.zeros((2, 2))
+        for traj in trajs: 
+            t1, t2, t3 = self._compute_suff_stats(traj) 
+            T1 += t1 
+            T2 += t2 
+            T3 += t3 
+        T3_inv = np.linalg.inv(T3)
+        Q = T2 @ T3_inv
+        Cov = (T1 - T2 @ T3_inv @ T2.T)/np.sum([traj.shape[-1] -1 for traj in trajs])
+
+        J = scipy.linalg.logm(Q)/dt 
+        c = scipy.linalg.solve_discrete_lyapunov(Q, Cov)
+        B = - J @ c - c @ J.T 
+        return J, B
+
+
+    def _compute_suff_stats(self, traj):
+        x_later = traj[:, 1:]
+        x_earlier = traj[:, :-1]
+        T1 = self._outer(x_later, x_later)
+        T2 = self._outer(x_later, x_earlier)
+        T3 = self._outer(x_earlier, x_earlier)
+        return T1, T2, T3 
+
+    def _outer(self, a, b): 
+            return np.einsum('ij,kj->ik', a, b)
--- utils.py
+++ utils.py
@@ -3,14 +3,6 @@ from matplotlib import pyplot as plt
 from scipy.io import loadmat
 import os
 
-def rescale(array): 
-    '''
-    Rescale by mean and std
-    '''    
-    m = np.mean(array)
-    s = np.std(array)
-    return (array-m)/s
-
 def chop(trajs): 
     '''
     Chop up trajectories to the same lengths as the shortest one 
@@ -24,16 +16,148 @@ def chop(trajs):
             start += L 
     return new_trajs
 
-
-def find_corr(x, y):
-    corr = np.correlate(x, y, mode='full')
-    L = x.size
-    n = np.arange(1, L+1)
-    return corr[:L]/n
-
-
-def find_auto_corr(x):
+def find_auto_corr(x, norm=True): 
     corr = np.correlate(x, x, mode='full')
     L = x.size
-    n = np.arange(1, L+1)
-    return (corr[:L]/n)[::-1]
+    n = np.arange(L, 0, -1)
+    corr = corr[L-1:]/n
+    if norm: 
+        corr /= corr[0]
+    return corr
+
+
+def find_corr(x, y, norm=True):
+    corr = np.correlate(x, y, mode='full')
+    L = x.size
+    n = np.concatenate([np.arange(1, L, 1), np.arange(L, 0, -1)])
+    corr /= n
+    if norm: 
+        corr /= np.sqrt(np.mean(x**2))*np.sqrt(np.mean(y**2))
+    return corr
+
+def plot_corr(data, N, dt, norm=True): 
+    t = dt*np.arange(N)
+    t1 = dt*np.arange(-N+1, N)
+    L = len(data[0][0])
+
+    fig, axes = plt.subplots(1, 3, figsize=(20, 5)) 
+    
+    theta_corrs = [] 
+    p_corrs = [] 
+    cross_corrs = [] 
+    for (theta, p) in data: 
+        theta_corrs.append(find_auto_corr(theta, norm=norm))
+        p_corrs.append(find_auto_corr(p, norm=norm))
+        cross_corrs.append(find_corr(theta, p, norm=norm))
+        axes[0].plot(t, theta_corrs[-1][:N])
+        axes[1].plot(t, p_corrs[-1][:N])
+        axes[2].plot(t1, cross_corrs[-1][L-N+1:L+N])
+    axes[0].set_ylim([-0.5, 1])
+    axes[0].set_xlim([0, 100])
+    axes[1].set_xlim([0, 100])
+    axes[0].set_ylabel('theta correlation')
+    axes[1].set_ylabel('p correlation')
+    axes[2].set_ylabel('cross correlation')
+    plt.show()
+        
+    fig, axes = plt.subplots(1, 3, figsize=(20, 5))
+    axes[0].plot(t, np.mean(theta_corrs, axis=0)[:N])
+    axes[1].plot(t, np.mean(p_corrs, axis=0)[:N])
+    axes[2].plot(t1, np.mean(cross_corrs, axis=0)[L-N+1:L+N])
+    axes[0].set_xlim([0, 100])
+    axes[1].set_xlim([0, 100])
+    axes[0].set_ylabel('theta correlation')
+    axes[1].set_ylabel('p correlation')
+    axes[2].set_ylabel('cross correlation')
+    plt.show() 
+    return theta_corrs, p_corrs, cross_corrs 
+
+def plot_corr_ft(data, dt): 
+    fig, axs = plt.subplots(1, 4, sharex=True, figsize=(15, 5))
+
+    omegas = np.fft.rfftfreq(len(data[0][0]))*2*np.pi
+    
+    for (theta, p) in data:
+        theta_omega = np.fft.rfft(theta)
+        p_omega = np.fft.rfft(p)
+        theta_corr = np.abs(theta_omega)**2
+        p_corr = np.abs(p_omega)**2
+        cross_corr = np.conj(theta_omega)*p_omega
+        
+        axs[0].plot(omegas, theta_corr, 'o--', alpha=0.5, color='orange')
+        axs[1].plot(omegas, p_corr, 'o--', alpha=0.5, color='orange')
+        axs[2].plot(omegas, cross_corr.real, 'o--', alpha=0.5, color='orange')
+        axs[3].plot(omegas, cross_corr.imag, 'o--', alpha=0.2, color='orange')
+    
+    theta_corr = np.mean(np.abs(np.fft.rfft(data[:, 0]))**2, axis=0)
+    axs[0].plot(omegas, theta_corr, color='red')
+    axs[0].set_title('theta auto corr')
+
+    p_corr = np.mean(np.abs(np.fft.rfft(data[:, 1]))**2, axis=0)
+    axs[1].plot(omegas, p_corr, color='red')
+    axs[1].set_title('polarisation auto corr')
+    
+    cross_corr = np.mean(np.conjugate(np.fft.rfft(data[:, 0]))*np.fft.rfft(data[:, 1]), axis=0)
+    axs[2].plot(omegas, cross_corr.real, 'x--', color='red')
+    
+    axs[2].set_title('cross corr real')
+
+    axs[3].plot(omegas, cross_corr.imag, color='red')
+    axs[3].set_title('cross corr imag')
+    
+    axs[0].set_xlim([0, max(omegas[:100])])
+    plt.tight_layout()
+    plt.show()  
+
+    return theta_corr, p_corr, cross_corr 
+
+def show(data): 
+    fig, axes = plt.subplots(1, len(data), sharex=True, sharey=True, figsize=(20, 3))
+
+    for (i, d) in enumerate(data): 
+        axes[i].plot(d[1], label='p')
+        axes[i].plot(d[0], label='theta')
+    axes[0].set_xlim([0, 5000])
+    plt.legend()
+    plt.show()
+
+def plot_dist(data):
+    fig, axes = plt.subplots(3, 8, sharey='row', sharex='row', figsize=(20, 8))
+    for (i, d) in enumerate(data): 
+        axes[0, i].hist2d(d[0], d[1], bins=20)
+        axes[0, i].set_xlabel('theta')
+        axes[0, i].set_ylabel('p')
+        axes[1, i].hist(d[1], bins=20, density=True)
+        axes[2, i].hist(d[0], bins=20, color='darkorange', density=True)
+    axes[1, 0].set_ylabel('p pdf')
+    axes[2, 0].set_ylabel('theta pdf')
+    plt.tight_layout()
+    plt.show() 
+
+
+def plot_overall_dist(data): 
+    thetas = np.concatenate([d[0] for d in data])
+    ps = np.concatenate([d[1] for d in data])
+
+    fig, axes = plt.subplots(1, 3, figsize=(10, 3))
+    axes[0].hist2d(thetas, ps)
+    axes[0].set_xlabel(r'$\theta$')
+    axes[0].set_ylabel(r'$p$')
+
+    axes[1].hist(ps, bins=20, density=True)
+    axes[1].set_xlim([-1, 1])
+    axes[1].set_xlabel(r'$p$')
+    axes[1].set_ylabel('pdf')
+    
+    axes[2].hist(thetas, bins=20, density=True, color='darkorange')
+    axes[2].set_xlim([-0.5, 0.5])
+    axes[2].set_xlabel(r'$\theta$')
+    axes[2].set_ylabel('pdf')
+    plt.tight_layout()
+    plt.show()
+
+def coarsen(array, N): 
+    L = int(len(array)/N)
+    reshaped_array = np.reshape(array[:L*N], (L, N))
+    return np.mean(reshaped_array, axis=-1)
+    
